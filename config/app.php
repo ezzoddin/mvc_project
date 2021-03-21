d383ac9f2203727ff1a0927cf60d5d788cb5d4c8
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2021-03-21 16:31:45 +0330

    Mvc kernel settings

---
+++ config/app.php
@@ -0,0 +1,17 @@
+<?php
+
+define('APP_TITLE', 'mvc project');
+define('BASE_URL', 'https://localhost:8080');
+define('BASE_DIR', realpath(__DIR__ . "/../"));
+
+$temporary = str_replace(BASE_URL, '', explode('?', $_SERVER['REQUEST_URI'][0]));
+$temporary === "/" ? $temporary = "" : $temporary = substr($temporary, 1);
+define('CURRENT_ROUTE', $temporary);
+
+global $routes;
+$routes = [
+    'get' => [],
+    'post' => [],
+    'put' => [],
+    'delete' => []
+];
